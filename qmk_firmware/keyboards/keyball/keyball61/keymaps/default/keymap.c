6f7dc3a7d0c7e115ef165ec1e32e052437dcea18
trtkhrmc <hiro.teratake@gmail.com>
2024-03-01 01:08:27 +0900

    Layer1,2 left side change

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/default/keymap.c
@@ -38,19 +38,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [1] = LAYOUT_universal(
-    _______  , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                                   _______  ,_______       , _______     , _______      , JP_CIRC, JP_YEN    ,
-    _______  , KC_BSPC  , KC_P7     , KC_P8     , KC_P9     ,KC_KP_SLASH   ,                                  _______  ,_______       , _______     , _______      , KC_HOME, KC_PGUP   ,
-    _______  , KC_LGUI  , KC_P4     , KC_P5     , KC_P6     ,KC_KP_MINUS,                                      _______  ,_______       ,LGUI(KC_UP)  , _______      , KC_END , KC_PGDN   ,
-    _______  , KC_LALT  , KC_P1     , KC_P2     , KC_P3     ,KC_KP_PLUS , KC_KP_ASTERISK   ,            _______,     _______  , LGUI(KC_LEFT),LGUI(KC_DOWN), LGUI(KC_RGHT),_______ , S(JP_BSLS),
-    _______  , KC_NUM_LOCK  , KC_P0 , KC_KP_DOT   , _______  , _______  , _______  ,          KC_DEL  ,     _______  ,_______       , _______     , _______      , _______, _______
+    _______, _______  , _______   , _______ , _______   , _______  ,                                       _______  ,_______       , _______     , _______      , JP_CIRC, JP_YEN    ,
+    _______, _______  , _______   , _______ , _______   , _______  ,                                          _______  ,_______       , _______     , _______      , KC_HOME, KC_PGUP   ,
+    _______, _______  , S(KC_LEFT), KC_UP   , S(KC_RGHT), _______  ,                                           _______  ,_______       ,LGUI(KC_UP)  , _______      , KC_END , KC_PGDN   ,
+    _______, _______  , KC_LEFT   , KC_DOWN , KC_RGHT   , _______  ,_______,                 _______,     _______  , LGUI(KC_LEFT),LGUI(KC_DOWN), LGUI(KC_RGHT),_______ , S(JP_BSLS),
+    _______, _______  , _______   ,_______  , _______   , _______  ,_______,                 KC_DEL  ,     _______  ,_______       , _______     , _______      , _______, _______
   ),
 
   [2] = LAYOUT_universal(
-    _______, _______  , _______   , _______ , _______   , _______  ,                                KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10  , KC_F11    ,
-    _______, _______  , _______   , _______ , _______   , _______  ,                                 KC_F12  , KC_BTN4  , _______  , KC_BTN5  ,  KC_HOME, KC_PGUP   ,
-    _______, _______  , S(KC_LEFT), KC_UP   , S(KC_RGHT), _______  ,                                _______  , KC_BTN1  , KC_UP    , KC_BTN2  , KC_END  , KC_PGDN   ,
-    _______, _______  , KC_LEFT   , KC_DOWN , KC_RGHT   , _______  ,_______,            _______   , _______  , KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  , _______  ,
-    _______, _______  , _______   ,_______  , _______   , _______  ,_______,              KC_DEL  , _______  , _______  , _______  , _______  , _______  , _______
+    _______  , KC_F1    , KC_F2    , KC_F3    , KC_F4     , KC_F5    ,                                KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10  , KC_F11    ,
+    _______  , KC_BSPC  , KC_P7     , KC_P8     , KC_P9   ,KC_KP_SLASH   ,                             KC_F12  , KC_BTN4  , _______  , KC_BTN5  ,  KC_HOME, KC_PGUP   ,
+    _______  , KC_LGUI  , KC_P4     , KC_P5     , KC_P6   ,KC_KP_MINUS,                               _______  , KC_BTN1  , KC_UP    , KC_BTN2  , KC_END  , KC_PGDN   ,
+    _______  , KC_LALT  , KC_P1     , KC_P2     , KC_P3   ,KC_KP_PLUS , KC_KP_ASTERISK,   _______   , _______  , KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  , _______  ,
+    _______  , KC_NUM_LOCK  , KC_P0 , KC_KP_DOT , _______ , _______  , _______  ,           KC_DEL  , _______  , _______  , _______  , _______  , _______  , _______
   ),
 
   [3] = LAYOUT_universal(
